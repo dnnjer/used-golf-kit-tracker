b09dbeeb95d6e4bdf7e1d06ba8b1fb59f783b703
dnnjer <ahmadbash2001@gmail.com>
2026-04-20 21:21:53 +0100

    Update golf_tracker.py

--- golf_tracker.py
+++ golf_tracker.py
@@ -1,11 +1,13 @@
+import os
+import json
 import requests
 from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 
-SHEET_NAME = "Golf Sets Tracker"
-GOOGLE_CREDS_FILE = "service_account.json"
-EBAY_ACCESS_TOKEN = "v^1.1#i^1#f^0#r^0#p^1#I^3#t^H4sIAAAAAAAA/+VYe2wURRjv9YUFyyM0gkDgWNBg6+3N7r1X7vTao7QE2tIrV2hC6tzubLt0b3fdmWt7PpuCGDBF0tBYwVAwvhKjxEQTSAmJGiBB1ATfj0jiG0jUBAyJSHD2Wsq1EkB6xCbeP5f55ptvft9vvsfsgK7CotLNVZvPF9sm5e7pAl25Nhs3BRQVFpRNzcudU5ADMhRse7oWd+V35/2yFMOEagj1CBu6hpG9M6FqWEgLg0zS1AQdYgULGkwgLBBRiIZXrRR4FgiGqRNd1FXGXh0JMpB3eSDHQ8B7EOD8gEq1yzYb9CDj9/jdUJLjcSgGgN8j0XmMk6hawwRqJMjwgPc6gNvBgwYuILh4gQuwfg/XxNhjyMSKrlEVFjChNFwhvdbMwHptqBBjZBJqhAlVhyujteHqyLKahqXODFuhYR6iBJIkHj2q0CVkj0E1ia69DU5rC9GkKCKMGWdoaIfRRoXwZTA3AT9NtYw4tyhxoo/3+n0AiFmhslI3E5BcG4clUSSHnFYVkEYUkroeo5SN+AYkkuFRDTVRHbFbf6uTUFVkBZlBZll5eF24ro4JhVsTUCqH2LEGI2m5rsqOuvqIw+cVOc7rhqKD47iA5Hd7hjcasjZM85idKnRNUizSsL1GJ+WIokZjueEzuKFKtVqtGZaJhShDj+cuc8gHmqxDHTrFJGnVrHNFCUqEPT28/gmMrCbEVOJJgkYsjJ1IU0TTyjAUiRk7mY7F4fDpxEGmlRBDcDo7OjrYDhermy1OHgDOuXbVyqjYihKQobpWrg/pK9df4FDSroiIrsSKQFIGxdJJY5UC0FqYkMfn4d1gmPfRsEJjpf8QZPjsHJ0R2coQiYceLk5zQ3bHJb/Pm40MCQ0HqdPCgeIw5UhAsw0RQ4Uicog0zpIJZCqS4PLIvMsvI4fkDcgOd0CWHXGP5HVwMkIAoXhcDPj/T4lyo6EeRaKJSFZiPWtxXt7UWbYislbHnfXmusZ1q7m2QHtlbFlnk+bzJKvCqxtFLDY2xoiH4OCNZsNVna9QFcpMA90/GwRYuZ49Eqp0TJA0Lveiom6gOl1VxNTEOmCXKdVBk6SiSFWpYFxOhg2jOju1Omvu/csycXN+Z69H/Uf96apeYStkJ5ZX1npMDUBDYa0OxIp6wmnlug7p9cMSN6dR26+qOEbJSWW0YYmIpX1JotfyNtZEUNI1NTUu3hR6851QrFE/h0hQpKErK5tmgsXtIvUY60nKAWZrrRtcg96GNNoPiamrKjJj3LjrQSKRJDCuoolWGLKQIAqcYM2a8/m8Ps7lcQXG5ZeYbsXNE62kWaU8v9t268t5PYJqYmL5bpi6lBStO+ot+ORwjn4ACeWkf1y37V3QbTuUa7OBpeAubhFYWJi3Jj/v9jlYIYhVoMxipUWj3/UmYttQyoCKmTsz59inX9bMH1zx6pYfZnU9udjZmzM14/1lz3owe+QFpiiPm5LxHAPmXZkp4KbNKua9wM0DLuDiuUATWHRlNp+7I78k2E52/HQo8HbLn883LV9bELh797lBUDyiZLMV5NBgyZmJy2MPfbA9xfezT8PUEaHx0t7HVv4297YTO058GJrXNzX88jdfFZdu+nGw5fBAf0dFbPr0i9s/Otlf9f6preTMqWm6/t6CDXU9hT3tfSWfbT2047XYPQt+nRtjnzmzsejO882Vivxib9X6Jfd3zX+45PUOuP/rw5OX7H5uzoPB3r/q/2go2R85uGtx2UtHd+18at+bPnnwtO3Y5+BS2ZrktsMzeo7iA7NrDhrvHH+8/Oe9A7sWtgvhnWdfCK060tcfnTy7894pG8/U9kXYLWe3Pfvohbc6WkOfzEiV2C4+ca7s2H1vzDi+sb7gEd/3paULpp0sPP3KF99+d+RgcKB0XxHzwKTe5gu1+b9v4gc+PjB0ln8Dq6S2tRkTAAA="
+SHEET_NAME = os.environ["SHEET_NAME"]
+EBAY_ACCESS_TOKEN = os.environ["EBAY_ACCESS_TOKEN"]
+GOOGLE_SERVICE_ACCOUNT_JSON = os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,7 +22,8 @@ SEARCH_TERMS = [
 MAX_PRICE_GBP = 500
 
 def connect_sheet():
-    creds = Credentials.from_service_account_file(GOOGLE_CREDS_FILE, scopes=SCOPES)
+    service_account_info = json.loads(GOOGLE_SERVICE_ACCOUNT_JSON)
+    creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
     client = gspread.authorize(creds)
     sheet = client.open(SHEET_NAME).sheet1
     return sheet
